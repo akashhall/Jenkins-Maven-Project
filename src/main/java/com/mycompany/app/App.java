108f86ae1f783ee2ef78e46cbf619ec86c794878
akashhall <41614662+akashhall@users.noreply.github.com>
2021-11-24 18:51:39 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World kya bolti public!!!!!" );
+        System.out.println( "Hello World kya bolti public" );
     }
 }
